2699fcb430d0543ea574b05e3a586abeddeefd45
Tomás Navarro <tomy.brm@gmail.com>
2021-11-26 21:37:28 -0300

    Agregando primera parte del proyecto TravelAgency

---
+++ src/main/controller/HomeController.java
@@ -0,0 +1,14 @@
+package main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String getHome() {
+		return "home";
+	}
+	
+}
