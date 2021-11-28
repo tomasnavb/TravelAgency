e6b9b51ccddaca3a46c9f6d21871d9690f79fcdb
Tomás Navarro <tomy.brm@gmail.com>
2021-11-27 22:41:38 -0300

    Ultima versión del día 27/11/2021

--- src/main/controller/HomeController.java
+++ src/main/controller/HomeController.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import main.model.Tour;
 
 @Controller
@@ -28,7 +30,10 @@ public class HomeController {
 		return "form";
 	}
 	@PostMapping("/processForm")
-	public String showTourData(@ModelAttribute Tour tour) {
+	public String showTourData(@Valid @ModelAttribute Tour tour, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "form";
+		}
 		tours.add(tour);
 		return "redirect:showOffer";
 	}
--- src/main/model/Tour.java
+++ src/main/model/Tour.java
@@ -4,16 +4,31 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Tour {
 	public enum Continent {
 		AFRICA, ASIA, EUROPE, NORTH_AMERICA, SOUTH_AMERICA;
 	}
 
+	@NotBlank(message = "Tour name can not be null")
+	@Size(min = 5, message = "Name should have at least 5 characters")
 	private String name;
+	@Pattern(regexp = "^[a-zA-Z]{2}-[0-9]{2}[a-zA-Z]{1}$", message = "Expected format is CC-DDC where C stands for character and D stands for digit")
 	private String code;
 	private Continent continent;
+	@NotNull(message="Please select the date")
+	@Future(message="Date should be from the future")
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date date;
+	@Min(value=7, message="Must be between 7 and 21")
+	@Max(value=21, message="Must be between 7 and 21")
 	private int duration;
 	private boolean allInclusive;
 
